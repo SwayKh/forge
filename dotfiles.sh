a303efae48b7a9612a022daf38671dea8e1b00ba
SwayKh <khatrisumit712@gmail.com>
2025-04-27 19:55:11 +0530

    Create dotfiles.sh script to clone my dotfiles and run linksym. Credit to typecraft-dev/crucible

---
+++ dotfiles.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+url="https://github.com/SwayKh/dotfiles"
+repo_dir="dotfiles"
+
+is_linksym_installed() {
+  pacman -Qi "linksym" &>/dev/null
+}
+
+if ! is_stow_installed; then
+  echo "Please install linksym to create symlinks from dotfiles directory"
+  exit 1
+fi
+
+cd "$HOME"
+
+# Check if the repository already exists
+if [ -d "$repo_dir" ]; then
+  echo "Repository '$repo_dir' already exists. Skipping clone"
+else
+  git clone "$url"
+fi
+
+# Check if the clone was successful
+if [ $? -eq 0 ]; then
+  cd "$repo_dir"
+  linksym update
+  linksym source
+else
+  echo "Failed to clone the repository."
+  exit 1
+fi
