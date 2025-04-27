8a69ed56a16a9482a1590845209ca6855f4ea5e3
SwayKh <khatrisumit712@gmail.com>
2025-04-27 23:33:18 +0530

    Create util.sh with function to check if program is installed or installing programs

---
+++ utils.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+# Credit to typecraft-dev/crucible for a lot of parts of this script
+
+# Function to check if a package is installed
+is_installed() {
+  pacman -Qi "$1" &>/dev/null
+}
+
+helper=""
+check_installed_helper() {
+  if pacman -Qi yay &>/dev/null; then
+    helper="yay"
+  elif pacman -Qi paru &>/dev/null; then
+    helper="paru"
+  else
+    echo "Yay or Paru are not installed!"
+    exit 1
+  fi
+}
+check_installed_helper
+
+install_packages() {
+  local packages=("$@")
+  local to_install=()
+
+  for pkg in "${packages[@]}"; do
+    if ! is_installed "$pkg"; then
+      to_install+=("$pkg")
+    fi
+  done
+
+  if [ ${#to_install[@]} -ne 0 ]; then
+    echo "Installing: ${to_install[*]}"
+    "$helper" -S --noconfirm "${to_install[@]}"
+  fi
+}
